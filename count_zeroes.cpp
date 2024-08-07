ff891f6358a5aa8751a2932e023df1193c258e13
Ashwini Somnath Unde <146004602+ashwiniunde@users.noreply.github.com>
2024-08-08 00:37:09 +0530

    Count_zeroes.cpp

--- count_zeroes.cpp
+++ count_zeroes.cpp
@@ -1,46 +1,48 @@
-#include<iostream>
+#include<iostream> //Calcilating no. of zeros in array using divide 
 using namespace std;
 int main()
 {
-    int a[20],n;
+    int a[20],n; // Array to store elements and variable for the number of elements
     int count=0;
     cout<<"Enter the no of element::";
     cin>>n;
     cout<<"Enter the element of array::";
     for(int i=0;i<n;i++)
    {
-    cin>>a[i];
+    cin>>a[i];  // Reading array elements
    }
-   int low=0;
-   int high=n-1;
+   int low=0; // Initialize the start of the search range
+   int high=n-1; // Initialize the end of the search range
    while(high>=low)
     {
         int mid=(low+high)/2;
         if(a[mid]==0)
         {
+             // Check if it is the first zero in the array
             if(a[mid-1]==1 ||  mid==0)
             {
-                count=n-mid;
+                count=n-mid;  // Count the number of zeros
                 break;
             }
             else
             {
-                high=mid-1;
+                high=mid-1; // Continue searching in the left half
             }
         }
 
         else
         {
-            low=mid+1;
+            low=mid+1;  // Continue searching in the right half
+
         }
     }
 
    if (count==0)
    {
-    cout<<"There is no zero in array"<<endl;
+    cout<<"There is no zero in array"<<endl; // No zeros found
    } 
    else
    {
-    cout<<"Number of zeros present are::"<<count;
+    cout<<"Number of zeros present are::"<<count; // Print the count of zeros
    }
 }
